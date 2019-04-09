2a440369e287074b5517a6ee45cd5c32b1239ebb
tyler bailey <baile320@umn.edu>
2019-04-09 17:41:40 -0500

    Again, more fixes for auth0

--- auth_config.js
+++ auth_config.js
@@ -6,7 +6,7 @@ const url = process.env.URI || 'localhost';
 const apiPort = process.env.PORT || process.env.API_PORT;
 const hostPort = process.env.PORT || process.env.HOST_PORT;
 const clientId = process.env.CLIENT_ID || 'usLDsrjOBiuUsEi1TV2Crh93Xn7kHhXv';
-const authDomain = process.env.AUTH_DOMAIN;
+const authDomain = process.env.AUTH_DOMAIN || 'dev-vxw7uzlp';
 
 module.exports.authConfig = {
   domain: authDomain,
