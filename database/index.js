cac613c4d7efbef17cf2f68793d64dffd54c859f
tyler bailey <baile320@umn.edu>
2019-04-09 13:25:28 -0500

    Configure db to allow for prod migration

--- database/index.js
+++ database/index.js
@@ -3,12 +3,27 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const dbHostName = process.env.DB_HOST || 'localhost';
-const dbPort = process.env.DB_PORT || 27017;
-const dbDatabaseName = process.env.DB_NAME || 'reminder';
+const options = { useNewUrlParser: true };
 
-const db = mongoose.connect(`mongodb://${dbHostName}:${dbPort}/${dbDatabaseName}`,
-  { useNewUrlParser: true })
+let connString;
+
+if (process.env.NODE_ENV !== 'production') {
+  const dbHostName = 'localhost';
+  const dbPort = process.env.DB_PORT || 27017;
+  const dbDatabaseName = process.env.DB_NAME || 'reminder';
+  connString = `mongodb://${dbHostName}:${dbPort}/${dbDatabaseName}`;
+} else {
+  const dbUrl = process.env.DB_URL;
+  connString = `mongodb+srv://${dbUrl}/test?retryWrites=true`;
+
+  const dbHostName = process.env.DB_HOST || 'localhost';
+  const dbPassword = process.env.DB_PASS || '';
+  options.user = dbHostName;
+  options.pass = dbPassword;
+}
+
+console.log(connString);
+const db = mongoose.connect(connString, options)
   .then(() => console.log('Successfully connected to Mongo'))
   .catch(err => console.log(err));
 
