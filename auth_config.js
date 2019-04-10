c31486d39fc669de23c471d34b8c379a1e4a710f
tyler bailey <baile320@umn.edu>
2019-04-09 19:39:20 -0500

    process.env isn't working for some reason

--- auth_config.js
+++ auth_config.js
@@ -5,13 +5,12 @@ dotenv.config();
 const url = process.env.URI || 'localhost';
 const apiPort = process.env.PORT || process.env.API_PORT;
 const hostPort = process.env.PORT || process.env.HOST_PORT;
-const clientId = process.env.CLIENT_ID || process.env.CLIENT_ID;
 const authDomain = process.env.AUTH_DOMAIN || 'dev-vxw7uzlp';
 
 module.exports.authConfig = {
   domain: authDomain,
   fullDomain: `${authDomain}.auth0.com`,
-  clientId,
+  clientId: process.env.CLIENT_ID || 'usLDsrjOBiuUsEi1TV2Crh93Xn7kHhXv',
   redirectUri: `https://tylerreminderapp.herokuapp.com/callback`,
   aud: `https://tylerreminderapp.herokuapp.com/api`,
   logoutUri: `https://tylerreminderapp.herokuapp.com/`,
